47dd093c6ffc5bc0eb3c95d3c6ebe75f53727a77
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-12-05 14:26:58 -0700

    past work

--- Week 17/17-NoSQL/01-Activities/03-Day/26-Stu-Mini-Project/Unsolved/public/db.js
+++ Week 17/17-NoSQL/01-Activities/03-Day/26-Stu-Mini-Project/Unsolved/public/db.js
@@ -1,8 +1,13 @@
 let db;
 // create a new db request for a "budget" database.
+let request = indexedDB.open('budget', 1);
+
 
 request.onupgradeneeded = function (event) {
   // create object store called "pending" and set autoIncrement to true
+  let db = event.target.result;
+  db.createObjectStore('pending', { autoIncrement: true });
+
 };
 
 request.onsuccess = function (event) {
@@ -15,18 +20,26 @@ request.onsuccess = function (event) {
 
 request.onerror = function (event) {
   // log error here
+  console.log('Something Went Wrong');
 };
 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
+  let transaction = db.transaction(['pending'], 'readwrite');
   // access your pending object store
+  let storing = transaction.objectStore('pending');
   // add record to your store with add method.
+  storing.add(record);
+
 }
 
 function checkDatabase() {
   // open a transaction on your pending db
+  let transaction = db.transaction(['pending'], 'readwrite');
   // access your pending object store
+  let storing = transaction.objectStore('pending');
   // get all records from store and set to a variable
+  let getAll = storing.getAll();
 
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
@@ -41,7 +54,10 @@ function checkDatabase() {
         .then((response) => response.json())
         .then(() => {
           // if successful, open a transaction on your pending db
+          let transaction = db.transaction(['pending'], 'readwrite');
           // access your pending object store
+          let storing = transaction.objectStore('pending');// access your pending object store
+          storing.clear();
           // clear all items in your store
         });
     }
